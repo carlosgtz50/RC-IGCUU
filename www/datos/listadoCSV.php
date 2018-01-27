1ba519dfaad85afbe1d6b62e3b959fb0abdc2240
Carlos Gutiérrez <carlosgtz50@gmail.com>
2018-01-26 18:57:48 -0700

    — prueba simple

--- www/datos/listadoCSV.php
+++ www/datos/listadoCSV.php
@@ -18,25 +18,27 @@ if(isset($_POST['frmidCategoria']) and !empty($_POST['frmidCategoria'])){
 
 $fila = 1;
 
-f (($gestor = fopen("file:///android_asset/www/BaseRegistro.csv", "r")) !== FALSE) {
+echo "Esta regresando datos";
 
-	echo $gestor;
+// f (($gestor = fopen("file:///android_asset/www/BaseRegistro.csv", "r")) !== FALSE) {
 
-    // while (($datos = fgetcsv($gestor, 0, ",")) !== FALSE) {
+// 	echo $gestor;
 
-    //     $fila++;
+//     // while (($datos = fgetcsv($gestor, 0, ",")) !== FALSE) {
+
+//     //     $fila++;
 
 
-    //     echo $datos[0];
-    //     break; 
+//     //     echo $datos[0];
+//     //     break; 
 
-    //     // for ($c=0; $c < $numero; $c++) {
-    //     //     echo $datos[$c] . "<br />\n";
-    //     // }
-    // }
+//     //     // for ($c=0; $c < $numero; $c++) {
+//     //     //     echo $datos[$c] . "<br />\n";
+//     //     // }
+//     // }
 
-    fclose($gestor);
-}
+//     fclose($gestor);
+// }
 
 // header('Content-type: application/json');
 // echo json_encode($return_arrayPersonas, JSON_PRETTY_PRINT);
